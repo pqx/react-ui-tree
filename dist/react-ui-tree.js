15c6c0f98e71262740a5353ade7e65c2b76fea10
Danny <dannav@users.noreply.github.com>
2015-10-18 12:51:33 -0400

    static tree func
    allow creation of static tree to prevent manipulation of structure if defined

--- dist/react-ui-tree.js
+++ dist/react-ui-tree.js
@@ -114,87 +114,89 @@ module.exports = React.createClass({
 
   // oh
   drag: function drag(e) {
-    if (this._start) {
-      this.setState({
-        dragging: this.dragging
-      });
-      this._start = false;
-    }
-
-    var tree = this.state.tree;
-    var dragging = this.state.dragging;
-    var paddingLeft = this.props.paddingLeft;
-    var newIndex = null;
-    var index = tree.getIndex(dragging.id);
-    var collapsed = index.node.collapsed;
-
-    var _startX = this._startX;
-    var _startY = this._startY;
-    var _offsetX = this._offsetX;
-    var _offsetY = this._offsetY;
-
-    var pos = {
-      x: _startX + e.clientX - _offsetX,
-      y: _startY + e.clientY - _offsetY
-    };
-    dragging.x = pos.x;
-    dragging.y = pos.y;
-
-    var diffX = dragging.x - paddingLeft / 2 - (index.left - 2) * paddingLeft;
-    var diffY = dragging.y - dragging.h / 2 - (index.top - 2) * dragging.h;
-
-    if (diffX < 0) {
-      // left
-      if (index.parent && !index.next) {
-        newIndex = tree.move(index.id, index.parent, 'after');
+    if (!this.props.static) {
+      if (this._start) {
+        this.setState({
+          dragging: this.dragging
+        });
+        this._start = false;
       }
-    } else if (diffX > paddingLeft) {
-      // right
-      if (index.prev) {
-        newIndex = tree.move(index.id, index.prev, 'append');
-      }
-    }
-
-    if (newIndex) {
-      index = newIndex;
-      newIndex.node.collapsed = collapsed;
-      dragging.id = newIndex.id;
-    }
-
-    if (diffY < 0) {
-      // up
-      var above = tree.getNodeByTop(index.top - 1);
-      newIndex = tree.move(index.id, above.id, 'before');
-    } else if (diffY > dragging.h) {
-      // down
-      if (index.next) {
-        var below = tree.getIndex(index.next);
-        if (below.children && below.children.length && !below.node.collapsed) {
-          newIndex = tree.move(index.id, index.next, 'prepend');
-        } else {
-          newIndex = tree.move(index.id, index.next, 'after');
+  
+      var tree = this.state.tree;
+      var dragging = this.state.dragging;
+      var paddingLeft = this.props.paddingLeft;
+      var newIndex = null;
+      var index = tree.getIndex(dragging.id);
+      var collapsed = index.node.collapsed;
+  
+      var _startX = this._startX;
+      var _startY = this._startY;
+      var _offsetX = this._offsetX;
+      var _offsetY = this._offsetY;
+  
+      var pos = {
+        x: _startX + e.clientX - _offsetX,
+        y: _startY + e.clientY - _offsetY
+      };
+      dragging.x = pos.x;
+      dragging.y = pos.y;
+  
+      var diffX = dragging.x - paddingLeft / 2 - (index.left - 2) * paddingLeft;
+      var diffY = dragging.y - dragging.h / 2 - (index.top - 2) * dragging.h;
+  
+      if (diffX < 0) {
+        // left
+        if (index.parent && !index.next) {
+          newIndex = tree.move(index.id, index.parent, 'after');
         }
-      } else {
-        var below = tree.getNodeByTop(index.top + index.height);
-        if (below && below.parent !== index.id) {
-          if (below.children && below.children.length) {
-            newIndex = tree.move(index.id, below.id, 'prepend');
+      } else if (diffX > paddingLeft) {
+        // right
+        if (index.prev) {
+          newIndex = tree.move(index.id, index.prev, 'append');
+        }
+      }
+  
+      if (newIndex) {
+        index = newIndex;
+        newIndex.node.collapsed = collapsed;
+        dragging.id = newIndex.id;
+      }
+  
+      if (diffY < 0) {
+        // up
+        var above = tree.getNodeByTop(index.top - 1);
+        newIndex = tree.move(index.id, above.id, 'before');
+      } else if (diffY > dragging.h) {
+        // down
+        if (index.next) {
+          var below = tree.getIndex(index.next);
+          if (below.children && below.children.length && !below.node.collapsed) {
+            newIndex = tree.move(index.id, index.next, 'prepend');
           } else {
-            newIndex = tree.move(index.id, below.id, 'after');
+            newIndex = tree.move(index.id, index.next, 'after');
+          }
+        } else {
+          var below = tree.getNodeByTop(index.top + index.height);
+          if (below && below.parent !== index.id) {
+            if (below.children && below.children.length) {
+              newIndex = tree.move(index.id, below.id, 'prepend');
+            } else {
+              newIndex = tree.move(index.id, below.id, 'after');
+            }
           }
         }
       }
-    }
-
-    if (newIndex) {
-      newIndex.node.collapsed = collapsed;
-      dragging.id = newIndex.id;
-    }
-
-    this.setState({
-      tree: tree,
-      dragging: dragging
-    });
+  
+      if (newIndex) {
+        newIndex.node.collapsed = collapsed;
+        dragging.id = newIndex.id;
+      }
+  
+      this.setState({
+        tree: tree,
+        dragging: dragging
+      });
+    };
   },
 
   dragEnd: function dragEnd() {
